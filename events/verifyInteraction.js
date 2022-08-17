78783298499f3192193c517c81e3711e925db961
LunarCodes <91988772+LunarTaku@users.noreply.github.com>
2022-08-17 20:54:00 +0300

    MAJOR UPDATE
    Added modals with verifecation codes, the verifecation code is fully customizable!

--- events/verifyInteraction.js
+++ events/verifyInteraction.js
@@ -6,9 +6,14 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
+  TextInputStyle,
+  TextInputBuilder,
+  ModalBuilder,
 } = require("discord.js");
 const { default: mongoose } = require("mongoose");
 const roleSchema = require("../../schemas/verifyRoleId");
+
+const randomString = require("randomized-string");
 
 module.exports = {
   name: "interactionCreate",
@@ -49,27 +54,106 @@ module.exports = {
     } else if (interaction.isButton()) {
       switch (interaction.customId) {
         case "verifyMember": {
-          const verifyRoleId = await roleSchema.findOne({ guildId: interaction.guild.id });
+          const verifyRoleId = await roleSchema.findOne({
+            guildId: interaction.guild.id,
+          });
+
           if (!verifyRoleId) {
             interaction.reply({
               content:
                 "You have not set a verification role yet! Use `/setrole` to set one!",
             });
+            return;
           }
 
-          const role = interaction.guild.roles.cache.get(verifyRoleId.roleId);
-          if (!role) return interaction.reply({ content: "Role not found!" });
+          var randomToken = randomString
+            .generate({ length: 5, charset: "hex" })
+            .toUpperCase();
 
-          await interaction.member.roles.add(role).then((m) => {
-            interaction
-              .reply({
-                content: "You have been verified!",
+          if (
+            interaction.member.roles.cache.some(
+              (role) => role.id === verifyRoleId.roleId
+            )
+          ) {
+            interaction.reply({
+              embeds: [
+                new EmbedBuilder()
+                  .setDescription(
+                    "You have already verified that you are a not robot!"
+                  )
+                  .setColor("Blue"),
+              ],
+              ephemeral: true,
+            });
+            return;
+          }
+
+          const modal = new ModalBuilder()
+            .setCustomId("verifyUserModal")
+            .setTitle(`Verification code: ${randomToken}`)
+            .setComponents(
+              new ActionRowBuilder().setComponents(
+                new TextInputBuilder()
+                  .setCustomId("veryUserInput")
+                  .setLabel("Verification code in title:")
+                  .setStyle(TextInputStyle.Short)
+                  .setRequired(true)
+                  .setMaxLength(5)
+              )
+            ); //
+
+          await interaction.showModal(modal);
+          const modalSubmitInt = await interaction
+            .awaitModalSubmit({
+              filter: (i) => {
+                return true;
+              },
+              time: 600000,
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+
+          if (
+            modalSubmitInt.fields
+              .getTextInputValue("veryUserInput")
+              .toUpperCase() === randomToken
+          ) {
+            const role = interaction.guild.roles.cache.get(verifyRoleId.roleId);
+
+            if (!role)
+              return interaction.reply({
+                content: "Role not found!",
                 ephemeral: true,
-              })
-              .catch((err) => {
-                console.log(err);
               });
-          });
+            await interaction.member.roles.add(role).then((m) => {
+              interaction
+                .followUp({
+                  embeds: [
+                    new EmbedBuilder()
+                      .setTitle("Verification Successful!")
+                      .setDescription(
+                        `You have been verified and have been given the ${role.name} role!`
+                      )
+                      .setColor("Green"),
+                  ],
+                  ephemeral: true,
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            });
+          }
+
+          if (
+            modalSubmitInt.fields
+              .getTextInputValue("veryUserInput")
+              .toUpperCase() !== randomToken
+          ) {
+            interaction.followUp({
+              content: "You have entered the wrong code!",
+            });
+          }
           break;
         }
 
@@ -79,3 +163,4 @@ module.exports = {
     }
   },
 };
+
